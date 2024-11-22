4250aaa26799292b410b7f15dfed2b5c1b862f1c
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2024-11-22 22:35:46 +0530

    added code for main

--- Chatbot_with_streamlit/main.py
+++ Chatbot_with_streamlit/main.py
@@ -51,8 +51,10 @@ def main():
     # Model selection
     model = st.sidebar.selectbox(
         'Choose a model',
-        ['mixtral-8x7b-32768', 'llama3-8b-8192', 'gemma-7b-it', 'gemma2-9b-it',
-         'llama3-groq-70b-8192-tool-use-preview', 'llava-v1.5-7b-4096-preview']
+        ['llama3-groq-70b-8192-tool-use-preview','llama3-groq-8b-8192-tool-use-preview',
+        'llama-3.2-11b-vision-preview','llama-3.2-1b-preview','llama-3.2-3b-preview','llama-3.2-90b-text-preview',
+        'llama-3.2-90b-vision-preview','llama-guard-3-8b','llama3-70b-8192', 
+        'mixtral-8x7b-32768', 'llama3-8b-8192','gemma-7b-it','gemma2-9b-it']
     )
 
     # Conversational memory length selection
